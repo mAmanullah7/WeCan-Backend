1275ab08cf82590dbb3148958d16e5f044e88011
Mohammad Amanullah <ullahaman072003@gmail.com>
2025-04-30 22:38:04 +0530

    created nextauth

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,31 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      role?: string;
+    };
+  }
+
+  interface User {
+    id: string;
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    role?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role?: string;
+  }
+}
